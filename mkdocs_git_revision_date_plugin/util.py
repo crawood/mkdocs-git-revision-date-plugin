f098e5261bfabf3a2e80dd1a4dea5c62bc9c5c76
crawood <monsbrot.anthonin@gmail.com>
2020-02-28 16:52:15 +0100

    Update util.py
    the function return the most recent date between commit and merge

--- mkdocs_git_revision_date_plugin/util.py
+++ mkdocs_git_revision_date_plugin/util.py
@@ -6,4 +6,10 @@ class Util:
         self.g = Git()
 
     def get_revision_date_for_file(self, path: str):
-        return self.g.log(path, n=1, date='short', format='%ad')
+        date_commit = self.g.log(path, n=1, date='short', format='%ad')
+        date_merge = self.g.log(path, '-1', '--min-parents=2', '--format=%ad', '--date=short')
+        
+        if(date_commit > date_merge):
+            return date_commit
+        else:
+            return date_merge
