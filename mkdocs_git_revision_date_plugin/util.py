d153401cd4ecf6aed633a21a58d9359dd2be85cb
crawood <monsbrot.anthonin@gmail.com>
2020-03-06 11:22:06 +0100

    Update util.py

--- mkdocs_git_revision_date_plugin/util.py
+++ mkdocs_git_revision_date_plugin/util.py
@@ -6,10 +6,5 @@ class Util:
         self.g = Git()
 
     def get_revision_date_for_file(self, path: str):
-        date_commit = self.g.log(path, n=1, date='short', format='%ad')
-        date_merge = self.g.log(path, '-1', '--min-parents=2', '--format=%ad', '--date=short')
-        
-        if(date_commit > date_merge):
-            return date_commit
-        else:
-            return date_merge
+        date_merge = self.g.log(path, '--min-parents'=2, 'n'=1, 'date'='short', 'format'='%ad')
+        return date_merge
